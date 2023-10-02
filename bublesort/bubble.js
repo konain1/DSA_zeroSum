5256070e51c1e3f13e2a0cb5345eb9e6fa2c3acb
konain1 <kaunainkasab@gmail.com>
2023-10-02 14:12:43 +0530

    isswapped prevented for unnecessery loop

--- bublesort/bubble.js
+++ bublesort/bubble.js
@@ -1,15 +1,20 @@
 
 
-let arr = [3,2,5,6,7,1,4]
+let arr = [6,1,2,3,4,5,]
 
 function bublesort(arr1){
-
+    let isSwapped;
     for(let i = arr1.length-1;i>0;i--){
-
+        
         for(let j = 0;j<i;j++){
             if(arr1[j] > arr[j+1]){
                 [arr1[j],arr1[j+1] ]= [arr1[j+1],arr1[j]]
+                isSwapped = true
             }
+        }
+        if(!isSwapped){
+            console.log(i)
+            break;
         }
     }
     return arr1
