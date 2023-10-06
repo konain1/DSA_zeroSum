8a8691dbaf31c0bf4c6aa89621bf8fe8882c6e89
konain1 <kaunainkasab@gmail.com>
2023-10-06 12:19:57 +0530

    palindrome without predefine methods

--- palindorme/app.js
+++ palindorme/app.js
@@ -1,13 +1,34 @@
 
 
 
+// function isPalindrome(str){
+
+//     let newStr = str.split("").reverse().join("")
+
+//     console.log(newStr)
+//     return str.toLowerCase() === newStr.toLowerCase()
+// }
+
+// let result  = isPalindrome('pop')
+// console.log(result)
+
+
 function isPalindrome(str){
 
-    let newStr = str.split("").reverse().join("")
+    let left = 0;
+    let right = str.length -1
 
-    console.log(newStr)
-    return str.toLowerCase() === newStr.toLowerCase()
+    while(left < right){
+        if(str[left].toLowerCase() !== str[right].toLowerCase()){
+            return false
+        }
+        left++;
+        right--;
+    }
+
+    return true
+
 }
 
-let result  = isPalindrome('pop')
-console.log(result)
+const res = isPalindrome('Levep')
+console.log(res)
